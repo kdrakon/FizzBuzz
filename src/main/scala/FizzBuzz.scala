e23495a90dab9714ca6d9fef313912ab7bba49da
Sean Policarpio <sean.policarpio@bibliocommons.com>
2015-04-15 16:08:35 -0400

    Sending to github

---
+++ src/main/scala/FizzBuzz.scala
@@ -0,0 +1,17 @@
+
+/**
+ * "I made this." ~Sean Policarpio
+ */
+object FizzBuzz extends App {
+
+  implicit class FizzyOrBuzzy(i: Int) {
+    def isFizz = i % 3 == 0
+    def isBuzz = i % 5 == 0
+    def toFizzBuzz = if (i.isFizz || i.isBuzz) Some(("Fizz".filter(_ => i.isFizz)).concat("Buzz".filter(_ => i.isBuzz))) else None
+  }
+
+  print(
+    (1 to 100).flatMap(i => i.toFizzBuzz orElse Some(i)).mkString(",")
+  )
+
+}
